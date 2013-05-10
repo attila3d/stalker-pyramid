8f34b19d5ec85193aab1bca568dca59f2300d9f9
Gulsen Yilmaz <gulsenyilmaz9@gmail.com>
2013-05-10 11:32:49 +0300

    * Add: Append department and append group pages has connected to database.   * Add: Timelog List page added.   * Add: New placeholders added for different type of object.   * Fix: Links between pages is created with redirectLink function which is     written in base.jinja2. goToLink.js is deprecated, but still there are some     codes that uses gotolink.js, they must be changed for next update.

---
+++ stalker/views/structure.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+# Stalker a Production Asset Management System
+# Copyright (C) 2009-2013 Erkan Ozgur Yilmaz
+# 
+# This file is part of Stalker.
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation;
+# version 2.1 of the License.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+from pyramid.security import authenticated_userid
+from pyramid.view import view_config
+import transaction
+
+from stalker.db import DBSession
+from stalker import User, Structure, FilenameTemplate
+
+import logging
+from stalker import log
+logger = logging.getLogger(__name__)
+logger.setLevel(log.logging_level)
+
+
+@view_config(
+    route_name='update_structure',
+    renderer='templates/structure/dialog_update_structure.jinja2',
+    permission='Update_Structure'
+)
+def update_structure(request):
+    """called when updateing a structure
+    """
+    
+    login = authenticated_userid(request)
+    user = User.query.filter_by(login=login).first()
+    
+    structure_id = request.matchdict['structure_id']
+    structure = Structure.query.filter_by(id=structure_id).first()
+    
+    if 'submitted' in request.params:
+        if request.params['submitted'] == 'update':
+            if 'name' in request.params and \
+                'custom_template' in request.params and \
+                'filename_templates' in request.params:
+                    
+                    # get all FilenameTemplates
+                    ft_ids = [
+                        int(ft_id)
+                        for ft_id in request.POST.getall('filename_templates')
+                    ]
+                    
+                    fts = [
+                        FilenameTemplate.query.filter_by(id=ft_id).first()
+                        for ft_id in ft_ids
+                    ]
+                    
+                    # update structure
+                    structure.name = request.params['name']
+                    structure.templates = fts
+                    structure.custom_template = \
+                        request.params['custom_template']
+                    structure.updated_by = user
+                    
+                    DBSession.add(structure)
+                    # TODO: update date_updated
+                        
+    return {'structure': structure}
+
+
+@view_config(
+    route_name='create_structure',
+    renderer='templates/structure/dialog_create_structure.jinja2',
+    permission='Create_Structure'
+)
+def create_structure(request):
+    """called when adding a structure
+    """
+    
+    login = authenticated_userid(request)
+    user = User.query.filter_by(login=login).first()
+    
+    if 'submitted' in request.params:
+        logger.debug('"submitted" in request.params')
+        if request.params['submitted'] == 'create':
+            logger.debug('Adding a new structure')
+            # create and add a new structure
+            if 'name' in request.params and \
+                'custom_template' in request.params and \
+                'filename_templates' in request.params:
+                logger.debug('all the parameters are in place')
+                with transaction.manager:
+                    # get all FilenameTemplates
+                    ft_ids = [
+                        int(ft_id)
+                        for ft_id in request.POST.getall('filename_templates')
+                    ]
+                    
+                    fts = [
+                        FilenameTemplate.query.filter_by(id=ft_id).first()
+                        for ft_id in ft_ids
+                    ]
+                    
+                    # create structure
+                    new_structure = Structure(
+                        name=request.params['name'],
+                        custom_template=str(request.params['custom_template']),
+                        templates=fts,
+                        created_by=user,
+                    )
+                    DBSession.add(new_structure)
+            else:
+                logger.debug('there are missing parameters in request')
+    else:
+        logger.debug('submitted is not in request.params')
+    
+    return {
+        'filename_templates': FilenameTemplate.query.all()
+    }
+
+
+@view_config(
+    route_name='get_structures',
+    renderer='json',
+    permission='Read_Structure'
+)
+def get_structures(request):
+    """returns all the structures in the database
+    """
+    return [
+        {
+            'id': structure.id,
+            'name': structure.name
+        }
+        for structure in Structure.query.all()
+    ]
---
+++ stalker/views/time_log.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+# Stalker a Production Asset Management System
+# Copyright (C) 2009-2013 Erkan Ozgur Yilmaz
+# 
+# This file is part of Stalker.
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation;
+# version 2.1 of the License.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+from pyramid.httpexceptions import HTTPOk
+from pyramid.security import authenticated_userid, has_permission
+from pyramid.view import view_config
+from stalker import Task, User, Studio, TimeLog
+
+from stalker import defaults
+
+import logging
+from stalker import log
+from stalker.db import DBSession
+from stalker.views import get_datetime
+
+logger = logging.getLogger(__name__)
+logger.setLevel(log.logging_level)
+
+@view_config(
+    route_name='dialog_create_time_log',
+    renderer='templates/time_log/dialog_create_time_log.jinja2',
+)
+def create_time_log_dialog(request):
+    """creates a create_time_log_dialog by using the given task
+    """
+    logger.debug('inside create_time_log_dialog')
+    
+    # get logged in user
+    logged_in_user_id = authenticated_userid(request)
+    logged_in_user = User.query.filter(User.id==logged_in_user_id).first()
+    
+    task_id = request.matchdict['task_id']
+    task = Task.query.filter(Task.task_id==task_id).first()
+    
+    studio = Studio.query.first()
+    if not studio:
+        studio = defaults
+    
+    return {
+        'studio': studio,
+        'logged_in_user': logged_in_user,
+        'task': task
+    }
+
+
+@view_config(
+    route_name='create_time_log'
+)
+def create_time_log(request):
+    """runs when creating a time_log
+    """
+    task_id = request.matchdict['task_id']
+    task = Task.query.filter(Task.id==task_id).first()
+    
+    #**************************************************************************
+    # collect data
+    resource_id = request.params.get('resource_id', None)
+    resource = User.query.filter(User.id==resource_id).first()
+    
+    start_date = get_datetime(request, 'start_date', 'start_time')
+    end_date = get_datetime(request, 'end_date', 'end_time')
+    
+    logger.debug('task_id     : %s' % task_id)
+    logger.debug('task        : %s' % task)
+    logger.debug('resource_id : %s' % resource_id)
+    logger.debug('start_date  : %s' % start_date)
+    logger.debug('end_date    : %s' % end_date)
+    
+    if task and resource and start_date and end_date:
+        # we are ready to create the time log
+        # TimeLog should handle the extension of the effort
+        time_log = TimeLog(
+            task=task,
+            resource=resource,
+            start=start_date,
+            end=end_date
+        )
+        DBSession.add(time_log)
+    
+    return HTTPOk()
+
+
+@view_config(
+    route_name='list_time_logs',
+    renderer='templates/time_log/content_list_time_logs.jinja2'
+)
+def list_time_logs(request):
+    """lists the time logs of the given task
+    """
+    task_id = request.matchdict['task_id']
+    task = Task.query.filter_by(id=task_id).first()
+    
+    time_logs = []
+    if task:
+        time_logs = task.time_logs
+    
+    return {
+        'task': task,
+        'time_logs': time_logs,
+        'has_permission': has_permission
+    }
+
+@view_config(
+    route_name='get_time_logs',
+    renderer='json'
+)
+def get_time_logs(request):
+    """returns all the Shots of the given Project
+    """
+    task_id = request.matchdict['task_id']
+    task = Task.query.filter_by(id=task_id).first()
+
+    time_logs = []
+
+    for time_log in TimeLog.query.filter_by(task_id=task_id).all():
+        time_logs.append({
+            'id': time_log.id,
+            'resource_id': time_log.resource_id,
+            'resource_name': time_log.resource.name,
+            'start_date' : time_log.start.strftime('%s'),
+            'end_date':time_log.end.strftime('%s')
+            # 'hours_to_complete': time_log.hours_to_complete,
+            # 'notes': time_log.notes
+        })
+
+
+    return time_logs
